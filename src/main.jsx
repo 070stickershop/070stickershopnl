0e42a303c8f85cd3d60fb286a06e311edc1fb3b2
070stickershop <jayden0701070@gmail.com>
2026-02-26 10:20:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/dashboard" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
